0d4cf4f5f29f00598a453d7b0ce955ea4f38af09
jmmunoza <jmmunoza@eafit.edu.co>
2024-03-11 18:31:55 -0500

    Challenges fixed

--- src/components/canvas/components/car/Car.java
+++ src/components/canvas/components/car/Car.java
@@ -6,8 +6,6 @@ import components.canvas.observers.CanvasKeyObserver;
 import components.canvas.shapes.ShapeComposite;
 import components.canvas.transformations.rotation.RotationAnticlockwise;
 import components.canvas.transformations.rotation.RotationClockwise;
-import components.canvas.transformations.scaling.ScalingZoom;
-import components.canvas.transformations.scaling.ScalingZoomOut;
 import components.canvas.transformations.translation.Translation;
 
 import java.awt.event.KeyEvent;
@@ -15,8 +13,8 @@ import java.io.FileNotFoundException;
 
 public class Car extends ShapeComposite implements CanvasKeyListener {
     private static final double ROTATION_DEGREES = 20;
-    private static final double SCALING_FACTOR = 1.15;
     private static final double TRANSLATION_FACTOR = 50;
+    private double angle = 0;
 
     public Car(String carPath) throws FileNotFoundException {
         add(new CoordsDrawer(carPath));
@@ -27,14 +25,43 @@ public class Car extends ShapeComposite implements CanvasKeyListener {
     @Override
     public void onKeyPressed(int code) {
         switch (code) {
-            case KeyEvent.VK_ADD -> transform(new ScalingZoom(getXCenter(), getYCenter(), SCALING_FACTOR));
-            case KeyEvent.VK_SUBTRACT -> transform(new ScalingZoomOut(getXCenter(), getYCenter(), SCALING_FACTOR));
-            case KeyEvent.VK_A -> transform(new Translation(-TRANSLATION_FACTOR, 0));
-            case KeyEvent.VK_W -> transform(new Translation(0, TRANSLATION_FACTOR));
-            case KeyEvent.VK_S -> transform(new Translation(0, -TRANSLATION_FACTOR));
-            case KeyEvent.VK_D -> transform(new Translation(TRANSLATION_FACTOR, 0));
-            case KeyEvent.VK_Q -> transform(new RotationAnticlockwise(getXCenter(), getYCenter(), ROTATION_DEGREES));
-            case KeyEvent.VK_E -> transform(new RotationClockwise(getXCenter(), getYCenter(), ROTATION_DEGREES));
+            case KeyEvent.VK_A -> rotateLeft();
+            case KeyEvent.VK_W -> moveForward();
+            case KeyEvent.VK_S -> moveBack();
+            case KeyEvent.VK_D -> rotateRight();
         }
+    }
+
+    private void moveForward() {
+        double x = TRANSLATION_FACTOR * Math.sin(Math.toRadians(angle));
+        double y = TRANSLATION_FACTOR * Math.cos(Math.toRadians(angle));
+
+        transform(new Translation(x, y));
+    }
+
+    private void moveBack() {
+        double x = -TRANSLATION_FACTOR * Math.sin(Math.toRadians(angle));
+        double y = -TRANSLATION_FACTOR * Math.cos(Math.toRadians(angle));
+
+        transform(new Translation(x, y));
+    }
+
+    private void rotateRight() {
+        transform(new RotationClockwise(getXCenter(), getYCenter(), ROTATION_DEGREES));
+
+        changeAngle(ROTATION_DEGREES);
+    }
+
+    private void rotateLeft() {
+        transform(new RotationAnticlockwise(getXCenter(), getYCenter(), ROTATION_DEGREES));
+
+        changeAngle(-ROTATION_DEGREES);
+    }
+
+    private void changeAngle(double degrees) {
+        angle += degrees;
+
+        if (angle > 360) angle -= 360;
+        else if (angle < 0) angle += 360;
     }
 }
--- src/components/canvas/components/house3d/House3D.java
+++ src/components/canvas/components/house3d/House3D.java
@@ -1,69 +1,65 @@
 package components.canvas.components.house3d;
 
 import components.canvas.components.coordsdrawer.CoordsDrawer;
-import components.canvas.listeners.CanvasKeyArrowListener;
 import components.canvas.listeners.CanvasKeyListener;
-import components.canvas.observers.CanvasKeyArrowObserver;
 import components.canvas.observers.CanvasKeyObserver;
 import components.canvas.shapes.ShapeComposite;
-import components.canvas.transformations.rotation.Rotation3DXClockwise;
 import components.canvas.transformations.rotation.Rotation3DYClockwise;
-import components.canvas.transformations.rotation.Rotation3DZClockwise;
-import components.canvas.transformations.scaling.Scaling3DZoom;
-import components.canvas.transformations.scaling.Scaling3DZoomOut;
 import components.canvas.transformations.translation.Translation3D;
 
 import java.awt.event.KeyEvent;
 import java.io.FileNotFoundException;
 
-public class House3D extends ShapeComposite implements CanvasKeyArrowListener, CanvasKeyListener {
+public class House3D extends ShapeComposite implements CanvasKeyListener {
     private static final double ROTATION_DEGREES = 20;
-    private static final double SCALING_FACTOR = 1.15;
     private static final double TRANSLATION_FACTOR = 50;
+    private double angle = 0;
 
     public House3D(String carPath) throws FileNotFoundException {
 
         add(new CoordsDrawer(carPath));
 
-        CanvasKeyArrowObserver.attachListener(this);
         CanvasKeyObserver.attachListener(this);
-    }
-
-    @Override
-    public void onUpPressed() {
-        transform(new Rotation3DXClockwise(getXCenter(), getYCenter(), getZCenter(), -ROTATION_DEGREES));
-    }
-
-    @Override
-    public void onDownPressed() {
-        transform(new Rotation3DXClockwise(getXCenter(), getYCenter(), getZCenter(), ROTATION_DEGREES));
-    }
-
-    @Override
-    public void onRightPressed() {
-        transform(new Rotation3DYClockwise(getXCenter(), getYCenter(), getZCenter(), ROTATION_DEGREES));
-    }
-
-    @Override
-    public void onLeftPressed() {
-        transform(new Rotation3DYClockwise(getXCenter(), getYCenter(), getZCenter(), -ROTATION_DEGREES));
     }
 
     @Override
     public void onKeyPressed(int code) {
         switch (code) {
-            case KeyEvent.VK_ADD ->
-                    transform(new Scaling3DZoom(getXCenter(), getYCenter(), getZCenter(), SCALING_FACTOR));
-            case KeyEvent.VK_SUBTRACT ->
-                    transform(new Scaling3DZoomOut(getXCenter(), getYCenter(), getZCenter(), SCALING_FACTOR));
-            case KeyEvent.VK_A -> transform(new Translation3D(-TRANSLATION_FACTOR, 0, 0));
-            case KeyEvent.VK_W -> transform(new Translation3D(0, TRANSLATION_FACTOR, 0));
-            case KeyEvent.VK_S -> transform(new Translation3D(0, -TRANSLATION_FACTOR, 0));
-            case KeyEvent.VK_D -> transform(new Translation3D(TRANSLATION_FACTOR, 0, 0));
-            case KeyEvent.VK_Q ->
-                    transform(new Rotation3DZClockwise(getXCenter(), getYCenter(), getZCenter(), ROTATION_DEGREES));
-            case KeyEvent.VK_E ->
-                    transform(new Rotation3DZClockwise(getXCenter(), getYCenter(), getZCenter(), -ROTATION_DEGREES));
+            case KeyEvent.VK_A -> rotateLeft();
+            case KeyEvent.VK_W -> moveForward();
+            case KeyEvent.VK_S -> moveBack();
+            case KeyEvent.VK_D -> rotateRight();
         }
+    }
+
+    private void moveForward() {
+        double x = TRANSLATION_FACTOR * Math.sin(Math.toRadians(angle));
+        double z = TRANSLATION_FACTOR * Math.cos(Math.toRadians(angle));
+
+        transform(new Translation3D(x, 0, z));
+    }
+
+    private void moveBack() {
+        double x = -TRANSLATION_FACTOR * Math.sin(Math.toRadians(angle));
+        double z = -TRANSLATION_FACTOR * Math.cos(Math.toRadians(angle));
+
+        transform(new Translation3D(x, 0, z));
+    }
+
+    private void rotateRight() {
+        transform(new Rotation3DYClockwise(getXCenter(), getYCenter(), getZCenter(), ROTATION_DEGREES));
+        changeAngle(ROTATION_DEGREES);
+    }
+
+    private void rotateLeft() {
+        transform(new Rotation3DYClockwise(getXCenter(), getYCenter(), getZCenter(), -ROTATION_DEGREES));
+        changeAngle(-ROTATION_DEGREES);
+    }
+
+    private void changeAngle(double degrees) {
+        angle += degrees;
+
+        if (angle > 360) angle -= 360;
+        else if (angle < 0) angle += 360;
     }
 }

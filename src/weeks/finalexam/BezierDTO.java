936af922163ca22d5b11f89701b80aaf13bea6b5
tomas <64665732+tomasCalletce@users.noreply.github.com>
2024-05-21 15:11:15 -0500

    feat: add constructor

--- src/weeks/finalexam/BezierDTO.java
+++ src/weeks/finalexam/BezierDTO.java
@@ -4,10 +4,16 @@ import math.point.Point3D;
 
 public class BezierDTO {
     double[][] points;
-    
+
     int u;
     int v;
 
     Point3D center;
     
+    public BezierDTO(double[][] points, int u, int v, Point3D center) {
+        this.points = points;
+        this.u = u;
+        this.v = v;
+        this.center = center;
+    }
 }
